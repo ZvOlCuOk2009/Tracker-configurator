bf34b9c52366ce63bf46ce2d32e3a52455dd3e60
Zvigun Alexandr <mircamnia@ukr.net>
2016-12-20 11:00:14 +0200

    Initial Commit

---
+++ Tracker configurator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tracker configurator
+//
+//  Created by Mac on 20.12.16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
